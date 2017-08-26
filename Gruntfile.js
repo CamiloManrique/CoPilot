bd1f31b68df55a9dac9ea4f4b93d9ed88d183b6a
Camilo Manrique <camilomanriquec@gmail.com>
2017-08-26 00:00:40 -0500

    Theme change working on dev

---
+++ Gruntfile.js
@@ -0,0 +1,123 @@
+// AdminLTE Gruntfile
+module.exports = function (grunt) {
+
+  'use strict';
+
+  grunt.initConfig({
+    watch: {
+      // If any .less file changes in directory "build/less/" run the "less"-task.
+      files: ["build/less/*.less", "build/less/skins/*.less", "dist/js/app.js"],
+      tasks: ["less", "uglify"]
+    },
+    // "less"-task configuration
+    // This task will compile all less files upon saving to create both AdminLTE.css and AdminLTE.min.css
+    less: {
+      // Production compresses version
+      production: {
+        options: {
+          // Whether to compress or not
+          compress: true
+        },
+        files: {
+          // compilation.css  :  source.less
+          "static/css/AdminLTE.min.css": "src/adminlte-build/AdminLTE.less",
+          "dist/static/css/AdminLTE.min.css": "src/adminlte-build/AdminLTE.less",
+
+          // Skin
+          "dist/css/skin-blue.min.css": "src/adminlte-build/skin-blue.less",
+          "static/css/skin-blue.min.css": "src/adminlte-build/skin-blue.less"
+        }
+      }
+    },
+    // Uglify task info. Compress the js files.
+    uglify: {
+      options: {
+        mangle: true,
+        preserveComments: 'some'
+      },
+      my_target: {
+        files: {
+          'dist/js/app.min.js': ['dist/js/app.js']
+        }
+      }
+    },
+    // Build the documentation files
+    includes: {
+      build: {
+        src: ['*.html'], // Source files
+        dest: 'documentation/', // Destination directory
+        flatten: true,
+        cwd: 'documentation/build',
+        options: {
+          silent: true,
+          includePath: 'documentation/build/include'
+        }
+      }
+    },
+
+    // Optimize images
+    image: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'build/img/',
+          src: ['**/*.{png,jpg,gif,svg,jpeg}'],
+          dest: 'dist/img/'
+        }]
+      }
+    },
+
+    // Validate JS code
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      core: {
+        src: 'dist/js/app.js'
+      },
+      demo: {
+        src: 'dist/js/demo.js'
+      },
+      pages: {
+        src: 'dist/js/pages/*.js'
+      }
+    },
+
+    // Validate CSS files
+    csslint: {
+      options: {
+        csslintrc: 'build/less/.csslintrc'
+      },
+      dist: [
+        'dist/css/AdminLTE.css'
+      ]
+    },
+
+    // Validate Bootstrap HTML
+    bootlint: {
+      options: {
+        relaxerror: ['W005']
+      },
+      files: ['pages/**/*.html', '*.html']
+    },
+
+    // Delete images in build directory
+    // After compressing the images in the build/img dir, there is no need
+    // for them
+    clean: {
+      build: ["build/img/*"]
+    }
+  });
+
+  // Load all grunt tasks
+
+  // LESS Compiler
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  // Linting task
+  grunt.registerTask('lint', ['jshint', 'csslint', 'bootlint']);
+
+  // The default task (running "grunt" in console) is "watch"
+  grunt.registerTask('default', ['watch']);
+
+};
